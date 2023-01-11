ada63e19b61e176f5b0fbb82dd8111e21bb78762
filippo.merante <filippo.merante@sparkfabrik.com>
2023-01-11 16:01:06 +0100

    refs platform/1819: init project

---
+++ examples/test.tfvars
@@ -0,0 +1,18 @@
+database_setup = [
+  {
+    database = "db1"
+    user     = "user1"
+  },
+  {
+    database = "db2"
+    user     = "user2"
+  },
+  {
+    database = "db3"
+    user     = "user3"
+  },
+  {
+    database = "db4"
+    user     = "user4"
+  }
+]
---
+++ scripts/execute_cloud_sql_proxy.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env sh
+
+if ! [ -x "$(command -v cloud_sql_proxy)" ]; then
+    echo "Error: cloud_sql_proxy is not installed." >&2
+    exit 1
+fi
+
+SERVICE="cloud_sql_proxy"
+
+if ! pgrep -x "$SERVICE" >/dev/null
+then
+    exec cloud_sql_proxy -instances="${CLOUDSDK_CORE_PROJECT}:${GCLOUD_PROJECT_REGION}:${CLOUDSQL_INSTANCE_NAME}"="tcp:0.0.0.0:${CLOUD_SQL_PROXY_PORT}" /dev/null 2>&1 &
+fi
+
+for j in $(seq 1 10); do
+    READY=$(bash -c 'exec 3<> /dev/tcp/${CLOUD_SQL_PROXY_HOST}/${CLOUD_SQL_PROXY_PORT};echo $?' 2>/dev/null)
+    if [ "$READY" -eq 0 ]; then
+        echo "Connection Ready"
+        break
+    fi
+    echo "Waiting for Cloud SQL Proxy to start... $j"
+    sleep 1s
+done
+
+if [ "$READY" -eq 1 ]; then
+    echo "Error: Problem to connect sql instance ${CLOUD_SQL_PROXY_HOST}"
+    exit 1
+fi
